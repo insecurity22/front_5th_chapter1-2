0cce0d8992bcf4d99575fec2b76dc71e6e1c3a38
insecurity22 <insecurity@naver.com>
2025-04-03 04:35:32 +0900

    feat: createVNode, 객체 형태의 가상 돔(vNode) 생성 함수 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,23 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: flattenChildren(children),
+  };
+}
+
+function flattenChildren(children) {
+  return children?.reduce((acc, child) => {
+    if (Array.isArray(child)) {
+      return acc.concat(flattenChildren(child));
+    }
+
+    if (child === null || child === undefined || typeof child === "boolean") {
+      return acc;
+    }
+
+    acc.push(child);
+
+    return acc;
+  }, []);
 }
