1c31a164b6bcc42f8b98adbab312caf8dfaf8597
insecurity22 <insecurity@naver.com>
2025-04-03 04:42:29 +0900

    feat: normalizeVNode, 컴포넌트 정규화 함수 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
+// normalized VNode(Virtual Node)
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    const component = vNode.type;
+    const children = vNode.children?.map((child) => normalizeVNode(child));
+    const props = { ...vNode.props, children };
+    return normalizeVNode(component(props));
+  }
+
   return vNode;
 }
