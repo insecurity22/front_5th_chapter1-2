ab5a42ff0a1e9fc34993e0344d47ac6d4e103c27
insecurity22 <insecurity@naver.com>
2025-04-04 09:18:42 +0900

    feat: Add eventManager

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,32 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventMap.forEach((_, eventType) => {
+    root.removeEventListener(eventType, handleEvent);
+    root.addEventListener(eventType, handleEvent);
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+function handleEvent(event) {
+  const handlers = eventMap.get(event.type);
+  if (!handlers) return;
+
+  const handler = handlers.get(event.target);
+  if (typeof handler === "function") {
+    handler(event);
+  }
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventMap.has(eventType)) {
+    eventMap.set(eventType, new WeakMap());
+  }
+
+  eventMap.get(eventType).set(element, handler);
+}
+
+export function removeEvent(element, eventType) {
+  if (!eventMap.has(eventType)) return;
+
+  eventMap.get(eventType).delete(element);
+}
