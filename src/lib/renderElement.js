c9940162744c8a2c85f77ff5051278b7aef56704
insecurity22 <insecurity@naver.com>
2025-04-07 06:16:52 +0900

    feat: Add updateElement

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,18 +1,23 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  const $element = createElement(normalizeVNode(vNode));
-  container.appendChild($element);
+  const newVNode = normalizeVNode(vNode);
+  const oldVNode = container._vNode;
 
-  if (container.firstChild) {
+  // 최초 렌더링시에는 createElement로 DOM을 생성하고
+  if (!oldVNode) {
+    const $element = createElement(newVNode);
+    container.appendChild($element);
+  } else {
     // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-    // updateElement(container, normalizeVNode(vNode));
-    container.replaceChild($element, container.firstChild);
+    updateElement(container, newVNode, oldVNode);
   }
+
+  // 현재 가상 DOM 저장
+  container._vNode = newVNode;
 
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
   setupEventListeners(container);
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,97 @@
 import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+/**
+ * 속성 비교, 변경된 속성만 수정
+ */
+function updateAttributes(target, originNewProps, originOldProps) {
+  if (!target || !originNewProps) return;
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  Object.entries(originNewProps).forEach(([key, value]) => {
+    if (key === "className") {
+      target.setAttribute("class", value);
+    } else if (key.startsWith("on") && typeof value === "function") {
+      const eventType = key.slice(2).toLowerCase();
+      /**
+       * onClick -> click
+       * onMouseEnter -> mouseenter
+       * onMouseLeave -> mouseleave
+       * onMouseMove -> mousemove
+       * onMouseDown -> mousedown
+       * onMouseUp -> mouseup
+       * onMouseOut -> mouseout
+       * ...
+       */
+      addEvent(target, eventType, value);
+    } else {
+      target.setAttribute(key, value);
+    }
+  });
+
+  /**
+   * 더 이상 필요하지 않은 속성이나 이벤트 리스너 제거
+   */
+  Object.entries(originOldProps).forEach(([key, oldValue]) => {
+    const isPropRemoved = originNewProps[key] === undefined;
+    if (isPropRemoved) {
+      if (key === "className") {
+        target.removeAttribute("class");
+      } else if (key.startsWith("on") && typeof oldValue === "function") {
+        const eventType = key.slice(2).toLowerCase();
+        removeEvent(target, eventType);
+      } else {
+        target.removeAttribute(key);
+      }
+    }
+  });
+}
+
+/**
+ * 트리 구조(노드 전체) 비교, 변경된 노드만 수정
+ * - oldNode : 이전 노드
+ * - newNode : 최신 노드
+ */
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  if (!oldNode) {
+    parentElement.appendChild(createElement(newNode));
+    return;
+  }
+
+  if (!newNode) {
+    parentElement.removeChild(parentElement.childNodes[index]);
+    return;
+  }
+
+  if (typeof newNode === "string") {
+    const textNode = parentElement.childNodes[index];
+    if (textNode.textContent !== newNode) {
+      textNode.textContent = newNode;
+    }
+    return;
+  }
+
+  const $element = parentElement.childNodes[index];
+  if (!$element) return;
+
+  if (newNode.type !== oldNode.type) {
+    parentElement.replaceChild(createElement(newNode), $element);
+    return;
+  }
+
+  updateAttributes($element, newNode.props, oldNode.props);
+
+  const newLength = newNode.children?.length;
+  const oldLength = oldNode.children?.length;
+  for (let i = 0; i < newLength || i < oldLength; i++) {
+    updateElement($element, newNode.children[i], oldNode.children[i], i);
+  }
+}
+
+/**
+ * 1) 얕은 복사(Shallow Copy)
+ * const newNode = {
+ *   type: "div",
+ *   props: { onClick: oldNode.props.onClick },
+ *   children: oldNode.children,
+ * }
+ */
