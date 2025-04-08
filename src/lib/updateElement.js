cfef9cf0494945f2d0f9a6dadca4979b15ddd26e
insecurity22 <insecurity@naver.com>
2025-04-09 00:08:08 +0900

    fix: updateAttributes 함수 undefined 처리

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -5,13 +5,31 @@ import { createElement } from "./createElement.js";
  * 속성 비교, 변경된 속성만 수정
  */
 function updateAttributes(target, originNewProps, originOldProps) {
-  if (!target || !originNewProps) return;
+  const newProps = originNewProps || {};
+  const oldProps = originOldProps || {};
 
-  Object.entries(originNewProps).forEach(([key, value]) => {
-    if (key === "className") {
+  /**
+   * 더 이상 필요하지 않은 속성이나 이벤트 리스너 제거
+   */
+  Object.entries(oldProps).forEach(([key, oldValue]) => {
+    const isPropRemoved = newProps[key] === undefined;
+    if (isPropRemoved) {
+      if (key === "className") {
+        target.removeAttribute("class");
+      } else if (key.startsWith("on") && typeof oldValue === "function") {
+        const eventType = key.slice(2).toLowerCase();
+        removeEvent(target, eventType);
+      } else {
+        target.removeAttribute(key);
+      }
+    }
+  });
+
+  Object.entries(newProps).forEach(([attr, value]) => {
+    if (attr === "className") {
       target.setAttribute("class", value);
-    } else if (key.startsWith("on") && typeof value === "function") {
-      const eventType = key.slice(2).toLowerCase();
+    } else if (attr.startsWith("on") && typeof value === "function") {
+      const eventType = attr.slice(2).toLowerCase();
       /**
        * onClick -> click
        * onMouseEnter -> mouseenter
@@ -24,57 +42,39 @@ function updateAttributes(target, originNewProps, originOldProps) {
        */
       addEvent(target, eventType, value);
     } else {
-      target.setAttribute(key, value);
-    }
-  });
-
-  /**
-   * 더 이상 필요하지 않은 속성이나 이벤트 리스너 제거
-   */
-  Object.entries(originOldProps).forEach(([key, oldValue]) => {
-    const isPropRemoved = originNewProps[key] === undefined;
-    if (isPropRemoved) {
-      if (key === "className") {
-        target.removeAttribute("class");
-      } else if (key.startsWith("on") && typeof oldValue === "function") {
-        const eventType = key.slice(2).toLowerCase();
-        removeEvent(target, eventType);
-      } else {
-        target.removeAttribute(key);
-      }
+      target.setAttribute(attr, value);
     }
   });
 }
 
 /**
- * 트리 구조(노드 전체) 비교, 변경된 노드만 수정
- * - oldNode : 이전 노드
- * - newNode : 최신 노드
+ * 요소를 업데이트합니다. (트리 구조(노드 전체) 비교, 변경된 노드만 수정)
+ * - newNode : 최신 가상 DOM 노드
+ * - oldNode : 이전 가상 DOM 노드
  */
 export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  const $element = parentElement?.childNodes?.[index];
+
   if (!oldNode) {
     parentElement.appendChild(createElement(newNode));
     return;
   }
 
   if (!newNode) {
-    parentElement.removeChild(parentElement.childNodes[index]);
+    parentElement.removeChild($element);
+    return;
+  }
+
+  if (newNode.type !== oldNode.type) {
+    parentElement.replaceChild(createElement(newNode), $element);
     return;
   }
 
   if (typeof newNode === "string") {
-    const textNode = parentElement.childNodes[index];
+    const textNode = $element;
     if (textNode.textContent !== newNode) {
       textNode.textContent = newNode;
     }
-    return;
-  }
-
-  const $element = parentElement.childNodes[index];
-  if (!$element) return;
-
-  if (newNode.type !== oldNode.type) {
-    parentElement.replaceChild(createElement(newNode), $element);
     return;
   }
 
