0c292a544b8f611c5bdb18fa72c8a2d4b07075e2
insecurity22 <insecurity@naver.com>
2025-04-09 02:25:20 +0900

    Updates

---
+++ assets/404-D05Mg52B.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";function o(){var a;const t="/front_5th_chapter1-2",c=`#/${(a=location.pathname)==null?void 0:a.replace(t,"")}`,e=`${t}${c}`;window.location.replace(e)}o();
